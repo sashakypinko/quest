bad178adf6039061743e390e646899e8bcdbd6a7
Dmitriy <dmitriy.s.biz@solid-sl.com>
2020-08-22 01:11:43 +0300

    added google map location

--- resources/js/components/GoogleMap.vue
+++ resources/js/components/GoogleMap.vue
@@ -17,12 +17,21 @@
                 latitude: parseFloat(this.lat)
             };
         },
-        methods: {},
+        methods: {
+            handleLocationError(browserHasGeolocation, infoWindow, pos, map) {
+                infoWindow.setPosition(pos);
+                infoWindow.setContent(browserHasGeolocation ?
+                    'Error: The Geolocation service failed.' :
+                    'Error: Your browser doesn\'t support geolocation.');
+                infoWindow.open(map);
+            }
+        },
         async mounted() {
             try {
                 const google = await gmapsInit();
                 const geocoder = new google.maps.Geocoder();
                 const map = new google.maps.Map(this.$el);
+                let infoWindow = new google.maps.InfoWindow;
                 let marker = new google.maps.Marker({
                     map: map,
                     draggable: true,
@@ -39,10 +48,48 @@
                     map.setCenter(results[0].geometry.location);
                     map.fitBounds(results[0].geometry.viewport);
                 });
+
+                let self = this;
+                navigator.geolocation.getCurrentPosition(function (position) {
+                    var pos = {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude
+                    };
+
+                    infoWindow.setPosition(pos);
+                    infoWindow.setContent('Location found.');
+                    infoWindow.open(map);
+                    map.setCenter(pos);
+                }, function () {
+                    self.handleLocationError(true, infoWindow, map.getCenter(), map);
+                });
             } catch (error) {
                 console.error(error);
             }
-        },
+
+            // let infoWindow = new google.maps.InfoWindow;
+            //
+            // // Try HTML5 geolocation.
+            // if (navigator.geolocation) {
+            //     let self = this;
+            //     navigator.geolocation.getCurrentPosition(function (position) {
+            //         var pos = {
+            //             lat: position.coords.latitude,
+            //             lng: position.coords.longitude
+            //         };
+            //
+            //         infoWindow.setPosition(pos);
+            //         infoWindow.setContent('Location found.');
+            //         infoWindow.open(map);
+            //         map.setCenter(pos);
+            //     }, function () {
+            //         self.handleLocationError(true, infoWindow, map.getCenter());
+            //     });
+            // } else {
+            //     // Browser doesn't support Geolocation
+            //     this.handleLocationError(false, infoWindow, map.getCenter());
+            // }
+        }
     }
 </script>
 
